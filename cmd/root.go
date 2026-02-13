9745016976a18addc8854f09f6c884598e8354ae
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2026-02-13 09:17:17 +0100

    Cover root cmd with tests

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "k8s-inspector",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "K8s inspector is a CLI tool gather Kubernetes information",
+	Long: `K8s inspector is a CLI tool to inspect Kubernetes clusters and resources.
+This tool is designed to help developers and devops engineers to quickly gather information about their Kubernetes clusters.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +41,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
